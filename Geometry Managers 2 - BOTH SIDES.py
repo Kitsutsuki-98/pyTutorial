2fdffd5467f333fe8e4eeb74187dd2c5fb4088da
Kitsutsuki-98 <kitsutsukazzi@gmail.com>
2021-09-29 08:21:48 +0200

    Geometry Managers

---
+++ Geometry Managers 2 - BOTH SIDES.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+frame1 = tk.Frame(master=window, width=200, height=100, bg="red")
+frame1.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame2 = tk.Frame(master=window, width=100, bg="white")
+frame2.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+frame3 = tk.Frame(master=window, width=50, bg="green")
+frame3.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+
+window.mainloop()
---
+++ Geometry Managers 2 - Fill.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+frame1 = tk.Frame(master=window, height=100, bg="red")
+frame1.pack(fill=tk.X)
+
+frame2 = tk.Frame(master=window, height=50, bg="yellow")
+frame2.pack(fill=tk.X)
+
+frame3 = tk.Frame(master=window, height=25, bg="blue")
+frame3.pack(fill=tk.X)
+
+window.mainloop()
+
+# Fill = tkl.X Per riempire secondo la X (orizzontale)
+# ANCHE CON RESIZE DELLA WINDOW!

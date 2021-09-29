b03074825b4756ebeb74d2771c90738ba80ba069
Kitsutsuki-98 <kitsutsukazzi@gmail.com>
2021-09-29 08:22:12 +0200

    Geometry Managers - Grid

---
+++ grid().py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+window = tk.Tk()
+
+for i in range(6):
+    for j in range(6):
+        frame = tk.Frame(
+            master=window,
+            relief=tk.RAISED,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j)
+        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
+        label.pack()
+
+window.mainloop()
+
+#Più Utilizzato e si capisce perche!
